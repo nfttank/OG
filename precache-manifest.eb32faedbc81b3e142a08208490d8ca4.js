e42182d27bcd4eec415f3ab15ec7348ef61fa9a8
nfttank <nfttank>
2021-11-28 00:13:49 +0100

    Updates

---
+++ precache-manifest.eb32faedbc81b3e142a08208490d8ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10fb3c101af039bcc8e4",
+    "url": "/OG/static/css/main.f85aee38.chunk.css"
+  },
+  {
+    "revision": "10fb3c101af039bcc8e4",
+    "url": "/OG/static/js/main.10fb3c10.chunk.js"
+  },
+  {
+    "revision": "9d4b5d6445f049e87385",
+    "url": "/OG/static/css/1.2a672430.chunk.css"
+  },
+  {
+    "revision": "9d4b5d6445f049e87385",
+    "url": "/OG/static/js/1.9d4b5d64.chunk.js"
+  },
+  {
+    "revision": "2f319d6014fd8fc9b822",
+    "url": "/OG/static/js/runtime~main.2f319d60.js"
+  },
+  {
+    "revision": "1d71b2eb4e65b3be3a11dc6eb8bcf149",
+    "url": "/OG/index.html"
+  }
+];
