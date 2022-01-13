264d3a21aa26829a07485e4c0dc79c003d1b8349
nfttank <nfttank>
2022-01-13 23:16:38 +0100

    Use ethers.js instead of web3.js

--- src/App.js
+++ src/App.js
@@ -1,93 +1,118 @@
 import React, { Component, useState } from 'react'
-import Web3 from 'web3'
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
 import './App.css'
-import OG from '../src/abis/OG.json'
+import OG from './abis/OG.json'
 import { Footer, Balance, Header, Rules } from './containers';
 import {  Navbar } from './components';
 import './App.css';
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+  constructor(props) {
+    super(props)
+    this.state = {
+      network: { name: 'rinkeby', id: 4 },
+      //network: { name: 'mainnet', id: 1 },
+      mintFunction: null,
+      connectFunction: null,
+      remainingMintsForWallet: 0,
+      connected: false,
+      soldOut: false,
+      signerAddress: '',
+      contract: null,
+      balance: 0,
+      totalSupply: 0,
+      maxPerWallet: 10,
+      walletLoaded: false,
+      featuredOg: { },
+      featuredOgExists: false,
+      ogTwitterUrl: 'https://twitter.com/og_nft_official',
+      discordUrl: 'https://discord.com/invite/kTvaHARW',
+      tankTwitterUrl: 'https://twitter.com/nfttank',
+      contractUrl: '',
+      storeUrl: '',
+      ownedOgs: [],
+    }
   }
 
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
+  async componentWillMount() {
+
+    const networkId = this.state.network.id
+    const networkName = this.state.network.name
+    const networkAddress = OG.networks[networkId].address
+
+    this.setState({ mintFunction: () => this.mintRandom(this) })
+    this.setState({ connectFunction: () => this.connect(this.state.network) })
+
+    if (networkId === 1) {
+      this.setState({ contractUrl: "https://etherscan.io/address/" + networkAddress })
+      this.setState({ storeUrl: "https://opensea.io/assets/" + networkAddress })
     }
     else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask.')
+      this.setState({ contractUrl: "https://" + networkName + ".etherscan.io/address/" + networkAddress })
+      this.setState({ storeUrl: "https://testnets.opensea.io/assets/" + networkAddress })
     }
   }
 
-  async loadBlockchainData() {
-    const web3 = window.web3
+  async connect(network) {
 
-    this.setState({ mintFunction: this.mintRandom })
+    const providerOptions = {
+      /* See Provider Options Section */
+    };
+    
+    const web3Modal = new Web3Modal({
+      network: this.state.network.name,
+      cacheProvider: true,
+      providerOptions
+    });
 
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    const instance = await web3Modal.connect();
 
-    const networkId = await web3.eth.net.getId()
-    const networkData = OG.networks[networkId]
-    if (networkData) {
+    const provider = new ethers.providers.Web3Provider(instance);
+    const signer = provider.getSigner();
+    
+    // The Contract object
+    const contract = new ethers.Contract(OG.networks[this.state.network.id].address, OG.abi, provider);
 
-      const abi = OG.abi
-      const contractAddress = networkData.address
-      const contract = new web3.eth.Contract(abi, contractAddress)
-      this.setState({ contract })
- 
-      if (networkId === 1) {
-        this.setState({ contractUrl: "https://etherscan.io/address/" + contract.address })
-        this.setState({ storeUrl: "https://opensea.io/assets/" + contract.address })
-      }
-      else {
-        this.setState({ contractUrl: "https://rinkeby.etherscan.io/address/" + contract.address })
-        this.setState({ storeUrl: "https://testnets.opensea.io/assets/" + contract.address })
-      }
+    this.setState({ provider: provider })
+    this.setState({ signer: signer })
+    this.setState({ signerAddress: await this.state.signer.getAddress()})
+    this.setState({ connected: this.state.signerAddress !== ''})
+    this.setState({ contract: contract })
 
-      const remainingMints = await this.getRemainingMintsForWallet()
-      this.setState({ remainingMintsForWallet: remainingMints })
+    const remainingMints = await this.getRemainingMintsForWallet()
+    this.setState({ remainingMintsForWallet: remainingMints })
 
-      await this.loadTokens()
-
-    } else {
-      window.alert('Smart contract not deployed to detected network.')
-    }
+    await this.loadTokens()
   }
 
   async loadTokens() {
 
-    const balance = await this.state.contract.methods.balanceOf(this.state.account).call()
+    const balance = await this.state.contract.balanceOf(this.state.signerAddress)
     this.setState({ balance: balance})
 
-    const totalSupply = await this.state.contract.methods.totalSupply().call()
+    const totalSupply = await this.state.contract.totalSupply()
     this.setState({ totalSupply: totalSupply })
     this.setState({ soldOut: totalSupply >= 10000})
 
     this.setState({ walletLoaded: true })
     
     const randomId = Math.floor(Math.random() * 10000)
-    const featuredSvg = await this.state.contract.methods.renderSvg(randomId).call()
+    const featuredSvg = await this.state.contract.renderSvg(randomId)
     this.setState({ featuredOg: { id: randomId, svg: featuredSvg }})
     
     // ownerOf might crash and abort the methods execution
     try {
-      const owner = await this.state.contract.methods.ownerOf(randomId).call()
+      const owner = await this.state.contract.ownerOf(randomId)
       this.setState({ featuredOgExists: owner !== '' })
     } catch {}
 
     this.setState({ ownedOgs: []})
 
     for (var i = 0; i < balance; i++) {
-      const ogId = await this.state.contract.methods.tokenOfOwnerByIndex(this.state.account, i).call()
-      const ogSvg = await this.state.contract.methods.renderSvg(ogId).call()
+      const ogId = await this.state.contract.tokenOfOwnerByIndex(this.state.signerAddress, i)
+      const ogSvg = await this.state.contract.renderSvg(ogId)
 
       this.setState({ ownedOgs: this.state.ownedOgs.concat({id: ogId, svg: ogSvg})})
     }
@@ -95,7 +120,7 @@ class App extends Component {
 
   getRemainingMintsForWallet = async() => {
     
-    const balance = await this.state.contract.methods.balanceOf(this.state.account).call()
+    const balance = await this.state.contract.balanceOf(this.state.signerAddress)
     
     let count = this.state.maxPerWallet - balance;
 
@@ -105,7 +130,7 @@ class App extends Component {
       return 5
     }
 
-    const totalSupply = await this.state.contract.methods.totalSupply().call()
+    const totalSupply = await this.state.contract.totalSupply()
     const available = 10000 - totalSupply
     if (count > available)
       return available
@@ -113,62 +138,25 @@ class App extends Component {
     return count
   }
 
-  mintRandom = async() => {
+  mintRandom = async(app) => {
 
-    const count = await this.getRemainingMintsForWallet()
 
-    const canMint = count > 0 && !this.state.soldOut
+    if (app.state.contract == null)
+      await app.connect(app.state.network);
+
+    if (app.state.contract == null)
+      return;
+
+    const count = await app.getRemainingMintsForWallet()
+
+    const canMint = count > 0 && !app.state.soldOut
 
     if (canMint) {
       const seed = new Date().getMilliseconds()
-      const suggested = await this.state.contract.methods.suggestFreeIds(count, seed).call()
-      await this.state.contract.methods.mint(suggested).send({ from: this.state.account })
+      const suggested = await app.state.contract.suggestFreeIds(count, seed)
+      //await app.state.contract.methods.mint(suggested).send({ from: app.state.signerAddress })
 
-      await this.loadTokens()
-    }
-
-    // this.state.contract.methods.mint(this.state.tokenInput).send({ from: this.state.account })
-    //   .once('receipt', (receipt) => {
-    //     // this.setState({ ogs: this.state.ogs.concat(tokens[i])})
-    //     // const svg = this.state.contract.methods.renderSvg(tokens[i]).send({ from: this.state.account })
-    //     // this.setState({ svgs: this.state.svgs.concat(svg)})
-    //   })
-
-    //await this.state.contract.methods.mint(this.state.tokenInput).send({ from: this.state.account })
-    // .once('receipt', (receipt) => {
-    //   window.alert(receipt)
-    //   this.setState({ ogs: this.state.ogs.concat(this.state.tokenInput)})
-
-    //   for (var i = 0; i < this.state.tokenInput.length; i++) {
-    //     const svg = this.state.contract.methods.renderSvg(this.state.tokenInput[i]).send({ from: this.state.account })
-    //     this.setState({ svgs: this.state.svgs.concat(svg)})
-    //   }
-    //})
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      mintFunction: null,
-      connectFunction: null,
-      remainingMintsForWallet: 0,
-      soldOut: false,
-      account: '',
-      contract: null,
-      balance: 0,
-      totalSupply: 0,
-      maxPerWallet: 10,
-      walletLoaded: false,
-      featuredOg: {},
-      featuredOgExists: false,
-      ogTwitterUrl: 'https://twitter.com/og_nft_official',
-      discordUrl: 'https://discord.com/invite/kTvaHARW',
-      tankTwitterUrl: 'https://twitter.com/nfttank',
-      contractUrl: '',
-      storeUrl: '',
-      ownedOgs: [],
-      tokenInput: [],
-      tags: [],
+      //await app.loadTokens()
     }
   }
 
@@ -180,7 +168,9 @@ class App extends Component {
             <Header data={this.state} />
           </div>
           <Rules />
-          <Balance data={this.state} />
+          {   this.state.connected &&
+                <Balance data={this.state} />
+          }
           <Footer data={this.state} />
         </div>
     );
