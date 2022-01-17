8abfa019166094a1bc4fe2c49845d6461634d57a
nfttank <nfttank>
2022-01-17 23:03:20 +0100

    Enable coloring of OG tokens

--- src/components/ogimg/OgImg.jsx
+++ src/components/ogimg/OgImg.jsx
@@ -14,7 +14,13 @@ const OgImg = (props) => {
       <div dangerouslySetInnerHTML={{__html:
         props.data.svg === undefined
         ? "" // otherwise "undefined" will pop up before the svg is rendered
-        : (props.data.svg + "").replace('height=\'1000\'', '').replace('width=\'1000\'', '') // eliminate height and width to do scaling
+        : (props.data.svg + "")
+          .replace('height=\'1000\'', '')             /* eliminate height and width to do scaling */
+          .replace('width=\'1000\'', '')              /* eliminate height and width to do scaling */
+          .replace(/back/g, "back" + props.data.id)   /* make color fills unique, otherwise svgs would reuse the fills of others on the pages */
+          .replace(/frame/g, "frame" + props.data.id)
+          .replace(/digit/g, "digit" + props.data.id)
+          .replace(/slug/g, "slug" + props.data.id)
       }} />
       </a>
   </div>
